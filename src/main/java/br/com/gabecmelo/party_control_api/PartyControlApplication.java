3dbd401a9335726726a02e89161b540ecd478e04
Gabe <contatogabemelo@gmail.com>
2025-08-07 13:11:59 -0300

    Spring initial setup

---
+++ src/main/java/br/com/gabecmelo/party_control_api/PartyControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabecmelo.party_control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabecmelo/party_control_api/PartyControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabecmelo.party_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
